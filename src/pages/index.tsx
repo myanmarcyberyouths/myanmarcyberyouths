6791aa123c82d559d9beb00ad941c81c4b89d813
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2023-04-20 01:33:49 +0630

    fix: gradient color in hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import {Inter} from 'next/font/google'
 import HeroSection from '../components/HeroSection'
 import ContactSection from "@/components/ContactSection";
 import FAQSection from "@/components/FAQSection";
+import NewLetter from "@/components/NewLetter";
 
 const inter = Inter({subsets: ['latin']})
 
